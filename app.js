80f715840fd6aabcf74a9dc73c480792c4363a63
MrSkippy <j.h.wester@gmail.com>
2014-12-13 16:04:25 +0100

    Got db working

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-var express = require('express'),
+var config = require('./config'),
+	express = require('express'),
 	bodyParser = require('body-parser'),
-	mongo = require("mongo"),
-	mongoose = require("mongoose"),
+	mongo = require('mongo'),
+	mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
 	app = express();
 
-var db = mongoose.createConnection(
-  'mongodb://pollme:p0llM3@proximus.modulusmongo.net:27017/viQ9uwod'
-);
+// Connect to the database
+mongoose.connect(config.credentials.mongodb_connection);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var questionSchema = new Schema({
+	hash: String, // e.g. COOLNODE
+	question: String, // e.g. 'Is Node.js cool stuff'?
+	answers: { 
+		positive: {type: String, default: 'Yes'}, 
+		negative: {type: String, default: 'No'}
+	},
+	votes: {
+		positive: Number, // e.g. 60,
+		negative: Number  // e.g. 40
+	},
+	owner: String, // e.g. 'John Doe'
+	dateAdded: {type: Date, default: Date.now},
+	pollEnded: {type: Boolean, default: false} // when true voting stopped.
+});
+//questionSchema.methods.vote = function() {
+//	
+//};
+var Question = mongoose.model('Question', questionSchema);
+
+// Query the database on the hash
+var getQuestionPromise = function(cleaned_hash) {
+	var query = Question.where({hash: cleaned_hash}),
+		promise = query.findOne().exec();
+	return promise;
+};
 
 // 
 app.use('/public', express.static('public'));
@@ -16,64 +46,74 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 /*
-	get  '/': show front page
-	get  '/question/:hash': gets question json, show answer page (voter)
-	put  '/question/:hash/vote/{vote}': sends answer, gets results json, show current results page (voter)
-	post '/question/{form values}': sends question, gets ok, show share page (asker)
-	get  '/question/:hash/votes/': gets results json, show results page (asker)
+	get  '/': FE shows front page
+	get  '/question/:hash': gets question json, FE shows answer page (voter)
+	put  '/question/:hash/vote/{vote}': sends answer, gets results json, FE shows current results page (voter)
+	post '/question/{form values}': sends question, gets ok, FE shows share page (asker)
+	get  '/question/:hash/votes/': gets results json, FE shows results page (asker)
 	
-	later: end question
+	TODO: later: end question
 */
 
 var router = express.Router();
 router.param('hash', function(req, res, next, id) {
-	// sample question, would actually fetch from DB, etc...
-	req.question = {
-		hash: id,
-		question: 'Do you like this',
-		answers: { 
-			positive: 'Yes',
-			negative: 'No'
-		},
-		votes: {
-			positive: 60,
-			negative: 40
-		},
-		owner: 'John Doe'
-	};
+	// Get the question based on the hash, (TODO: clean it and) put it to uppercase
+	var cleaned_hash = id.toUpperCase();
+	//console.log('cleaned hash = '+cleaned_hash);
+	
+	// Create a promise for the question from the DB
+	req.promiseQuestion = getQuestionPromise(cleaned_hash);
+	
 	next();
 });
 router.route('/question/:hash?')
 	.all(function(req, res, next) {
-		// default logic for all
-		console.log('/question/ hash=' + req.params.hash);
+		// default logic for all, it does some logging...
+		console.log(req.method + ' ' + req.originalUrl + ' matched by ' + req.route.path + ' with hash: ' + req.params.hash);
+		if (req.method == 'POST') {
+			console.log(req.body);
+		}
 		next();
 	})
 	.get(function(req, res, next) {
-		console.log('GET');
-		console.log(req.params);
-		res.json(req.question);
+		// e.g. GET /question/coolnode
+		req.promiseQuestion.then(function(question) {
+			// Returns question JSON
+			res.json(question.toObject());
+		}).then(function(error) { // error
+			throw new Error(error + ' is an error!');
+		});
 	})
 	.put(function(req, res, next) {
 		next(new Error('not implemented'));
 	})
 	.post(function(req, res, next) {
-		console.log('POST');
-		console.log(req.body);
-		
-		// inserting the question
-		req.question = {};
-		req.question.hash = req.body.hash;
-		req.question.question = req.body.question;
-		req.question.owner = req.body.owner;
-		// save question ... etc
-		
-		res.json(req.question);
-		//next();
+		// POST /question
+		// Create a new question from the form values
+		var new_question = new Question({
+			hash: req.body.hash,
+			question: req.body.question,
+			owner: req.body.owner
+		});
+		// Save question to DB
+		new_question.save(function(error, question) {
+			console.log(question);
+			var status = {
+				success: true,
+				result: question
+			};
+			if (error) {
+				status.success = false;
+				throw new Error(error + ' is an error!');
+			}
+			// Returns status JSON
+			res.json(status);
+		});
 	})
 	.delete(function(req, res, next) {
 		next(new Error('not implemented'));
 	});
+// TODO: votes logic
 router.route('/question/:hash/votes?')
 	.all(function(req, res, next) {
 		// default logic for all
@@ -98,12 +138,15 @@ app.use(router);
 
 app.get('/', function(req, res) {
 	console.log('Request received: ' + req.url);
+	// TODO: serve angular based index.html
 	res.send('Hello World!');
 });
 
 var server = app.listen(process.env.PORT || 8076, function () {
-	var host = server.address().address,
-		port = server.address().port;
-		console.log(server.address());
-		console.log('PollMe app listening at http://%s:%s', host, port);
+	var //host = server.address().address,
+		port = server.address().port,
+		node_env = process.env.NODE_ENV || 'development';
+		//console.log(server.address());
+		//console.log('PollMe app listening at http://%s:%s', host, port);
+		console.log('* Poll Me app (%s) running and listening on port %s', node_env, port);
 });
